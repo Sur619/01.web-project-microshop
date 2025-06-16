c21e01deaf93e3725bd78945e3d0f69c40add05c
Sur619 <whatyoulookingfor619@gmail.com>
2025-06-16 23:39:43 +0300

    add API versioning and scoped session management

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -1,4 +1,11 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from asyncio import current_task
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+    AsyncSession,
+)
 
 from core.config import settings
 
@@ -15,6 +22,24 @@ class DatabaseHelper:
             autoflush=False,
             autocommit=False,
         )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,  # Use current_task to scope the session to the current async task
+        )
+
+        return session
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as sess:
+            yield sess
+            await sess.close()
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.get_scoped_session()
+        yield session
+        await session.close()
 
 
 db_helper = DatabaseHelper(
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
+from core.config import settings
 from core.models import Base, db_helper
 from items_views import router as items_router
 from users.views import router as users_router
+from api_v1 import router as router_v1
 
 
 @asynccontextmanager
@@ -18,7 +20,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(items_router)
 app.include_router(users_router)
-
+app.include_router(router_v1, prefix=settings.api_v1_prefix)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
