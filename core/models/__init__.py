2fa811bf1224d81825e25c2992d9b350e0d3fbbd
Sur619 <whatyoulookingfor619@gmail.com>
2025-06-19 23:20:01 +0300

    add test-CRUD operations for user and profile management

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,7 +1,18 @@
-__all__ = ("Base", "Product", "db_helper", "DatabaseHelper", "User", "Post")
+__all__ = (
+    "Base",
+    "Product",
+    "db_helper",
+    "DatabaseHelper",
+    "User",
+    "Post",
+    "Profile",
+    "UserRelationMixin",
+)
 
 from .base import Base
 from .product import Product
 from .db_helper import db_helper, DatabaseHelper
 from .user import User
 from .post import Post
+from .profile import Profile
+from .mixin import UserRelationMixin
