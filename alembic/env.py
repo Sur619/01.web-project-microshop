19f6009263980a8fd897be6b5dbde69d41d99f28
Sur619 <whatyoulookingfor619@gmail.com>
2025-06-19 23:19:10 +0300

    refactor database configuration to use nested DBSettings class

--- alembic/env.py
+++ alembic/env.py
@@ -14,7 +14,7 @@ from core.config import settings
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.db_url)
+config.set_main_option("sqlalchemy.url", settings.db.url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- core/config.py
+++ core/config.py
@@ -1,14 +1,20 @@
 from pydantic_settings import BaseSettings
 from pathlib import Path
+from pydantic import BaseModel
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+DB_PATH = BASE_DIR / "db.sqlite3"
+
+
+class DBSettings(BaseModel):
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    echo: bool = True
 
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
-    db_echo: bool = True
+    db: DBSettings = DBSettings()
 
 
 settings = Settings()
