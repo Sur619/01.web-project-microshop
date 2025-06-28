c3ed3db877090e00069cffd4024f263b4d6147c4
Sur619 <whatyoulookingfor619@gmail.com>
2025-06-28 15:00:15 +0300

    refactor JWT encoding/decoding functions to use settings and add expiration handling

--- auth/auth/utils.py
+++ auth/auth/utils.py
@@ -1,26 +1,45 @@
-import jwt
+import uuid
+from datetime import datetime, timedelta
+
 import bcrypt
-from core.config import AuthJWT
+import jwt
+
+from core.config import settings
+
+# >>> private_key = b"-----BEGIN PRIVATE KEY-----\nMIGEAgEAMBAGByqGSM49AgEGBS..."
+# >>> public_key = b"-----BEGIN PUBLIC KEY-----\nMHYwEAYHKoZIzj0CAQYFK4EEAC..."
 
 
 def encode_jwt(
-    payload,
-    key,
-    algorithm,
-):
+    payload: dict,
+    private_key: str = settings.auth_jwt.private_key_path.read_text(),
+    algorithm: str = settings.auth_jwt.algorithm,
+    expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+    expire_timedelta: timedelta | None = None,
+) -> str:
+    to_encode = payload.copy()
+    now = datetime.utcnow()
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+    to_encode.update(
+        exp=expire,
+        iat=now,
+    )
     encoded = jwt.encode(
-        payload,
-        key,
+        to_encode,
+        private_key,
         algorithm=algorithm,
     )
     return encoded
 
 
 def decode_jwt(
-    token,
-    public_key,
-    algorithm,
-):
+    token: str | bytes,
+    public_key: str = settings.auth_jwt.public_key_path.read_text(),
+    algorithm: str = settings.auth_jwt.algorithm,
+) -> dict:
     decoded = jwt.decode(
         token,
         public_key,
@@ -29,7 +48,9 @@ def decode_jwt(
     return decoded
 
 
-def hash_password(password: str) -> bytes:
+def hash_password(
+    password: str,
+) -> bytes:
     salt = bcrypt.gensalt()
     pwd_bytes: bytes = password.encode()
     return bcrypt.hashpw(pwd_bytes, salt)
